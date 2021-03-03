41a28756df5774fdd8961d728f49b4cfbd44b94f
jonny <carlos_garcia_sistemas@outlook.com>
2021-03-03 14:57:12 -0600

    Se une menu con action en la clase Main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
+import static UserInterface.Ui.action;
+import static  UserInterface.Ui.welcome;
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
+    int elec;
+
+    int op = welcome();
+
+    if (op == 1){
+        action();
+
+    }else if( op == 2){
+            System.out.println("Bye");
+        }else {
+        System.out.println("Ingresa un valor correcto");
+    }
+
 
     }
 }
--- src/UserInterface/Ui.java
+++ src/UserInterface/Ui.java
@@ -14,10 +14,24 @@ public class Ui {
 
         Scanner Welcome = new Scanner(System.in);
         System.out.println("Bienvenido a Iventory System ");
+        System.out.println();
         System.out.println("Te recomendamos seguir las indicaciones ingresando los numeros en las opciones ");
         System.out.println("Presiona 1 para continuar o 2 para salir ");
         op = Welcome.nextInt();
+
         return op;
     }
 
-}
+    public  static void action(){
+        Scanner elec = new Scanner(System.in);
+        System.out.println("Ingresa que deseas hacer  ");
+        System.out.println();
+        System.out.println("1 Ingresar mercancia  ");
+        System.out.println("2 Descontar mercancia");
+        op = elec.nextInt();
+    }
+
+    }
+
+
+
