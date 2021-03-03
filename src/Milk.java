9d48542cef059b3a98e1c90a83ca785b0dd9c216
jonny <carlos_garcia_sistemas@outlook.com>
2021-03-03 14:11:48 -0600

    Se modifica clase Milk adjuntando los metodos de Inventory

--- src/Milk.java
+++ src/Milk.java
@@ -5,4 +5,18 @@ public class Milk extends Inventory{
         super(description, price, exit, cant);
     }
 
+    @Override
+    public int ingresoMerc(int ingreso) {
+        return super.ingresoMerc(ingreso);
+    }
+
+    @Override
+    public int desconMerca(int salida) {
+        return super.desconMerca(salida);
+    }
+
+    @Override
+    public void setCant(int cant) {
+        super.setCant(cant);
+    }
 }
