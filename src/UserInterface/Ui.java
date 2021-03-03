1e7f748342f15545decdda5e90edc116fd02529a
jonny <carlos_garcia_sistemas@outlook.com>
2021-03-03 14:21:10 -0600

    Se crea primer parte de menu en la clase Ui

--- src/UserInterface/Ui.java
+++ src/UserInterface/Ui.java
@@ -1,4 +1,23 @@
 package UserInterface;
 
+import java.util.Scanner;
+
 public class Ui {
+    static int op;
+
+    /**
+     * Menu principal donde
+     * donde op se es evaludao para continuar o terminar el progra
+     * */
+
+    public static int  welcome(){
+
+        Scanner Welcome = new Scanner(System.in);
+        System.out.println("Bienvenido a Iventory System ");
+        System.out.println("Te recomendamos seguir las indicaciones ingresando los numeros en las opciones ");
+        System.out.println("Presiona 1 para continuar o 2 para salir ");
+        op = Welcome.nextInt();
+        return op;
+    }
+
 }
